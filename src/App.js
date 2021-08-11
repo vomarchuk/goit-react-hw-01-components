5611a569717452dcae6ddef55a156bc1c4f5ea9f
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2021-08-11 09:54:53 +0200

    правка путей в импортах

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import 'App.css';
 import Profile from 'components/Profile/Profile';
 import user from './components/Profile/user.json';
 
-import Statidtics from 'components/Statistics/StatidticsList';
+import Statidtics from './components/Statistics/StatidticsList';
 import dataStatistic from './components/Statistics/statistical-data.json';
 
-import FriendList from 'components/FriendList/FriendList';
+import FriendList from './components/FriendList/FriendList';
 import friends from './components/FriendList/friends.json';
 
-import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import transactions from './components/TransactionHistory/transactions.json';
 
 function App() {
