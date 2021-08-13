182ba05a49dd957c9dd8a9b3a27398a1547ffbac
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2021-08-13 20:01:21 +0200

    correction

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import 'App.css';
 import Profile from './components/Profile/Profile';
-import user from './components/Profile/user.json';
+import user from './data/user.json';
 
 import Statistics from './components/Statistics/StatisticsList';
-import dataStatistic from './components/Statistics/statistical-data.json';
+import dataStatistic from './data/statistical-data.json';
 
 import FriendList from './components/FriendList/FriendList';
-import friends from './components/FriendList/friends.json';
+import friends from './data/friends.json';
 
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import transactions from './components/TransactionHistory/transactions.json';
+import transactions from './data/transactions.json';
 
 function App() {
   return (
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,46 +2,31 @@ import s from './Profile.module.css';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-class Profile extends React.Component {
-  constructor({ name, tag, location, avatar, stats }) {
-    super({ name, tag, location, avatar, stats });
-    this.name = name;
-    this.tag = tag;
-    this.location = location;
-    this.avatar = avatar;
-    this.stats = stats;
-  }
-
-  render() {
-    return (
-      <div className={s.profile}>
-        <div className={s.description}>
-          <img
-            src={this.avatar}
-            alt="Аватар пользователя"
-            className={s.avatar}
-          />
-          <p className={s.name}>{this.name}</p>
-          <p className={s.tag}>@{this.tag}</p>
-          <p className={s.location}>{this.location}</p>
-        </div>
-        <ul className={s.stats}>
-          <li className={s.item}>
-            <span className={s.label}>Followers</span>
-            <span className={s.quantity}>{this.stats.followers}</span>
-          </li>
-          <li className={s.item}>
-            <span className={s.label}>Views</span>
-            <span className={s.quantity}>{this.stats.views}</span>
-          </li>
-          <li className={s.item}>
-            <span className={s.label}>Likes</span>
-            <span className={s.quantity}>{this.stats.likes}</span>
-          </li>
-        </ul>
+function Profile({ name, tag, location, avatar, stats }) {
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt="Аватар пользователя" className={s.avatar} />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
-    );
-  }
+      <ul className={s.stats}>
+        <li className={s.item}>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{stats.followers}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{stats.views}</span>
+        </li>
+        <li className={s.item}>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 Profile.propTypes = {
